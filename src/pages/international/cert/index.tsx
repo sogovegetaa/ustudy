46dea95d92e3a4ba63a0bde295f651a965ca1b5a
kuaysh <mustakhkuanysh@gmail.com>
2023-01-19 17:54:55 +0600

    add required for forms

--- src/pages/international/cert/index.tsx
+++ src/pages/international/cert/index.tsx
@@ -63,7 +63,8 @@ function Cert() {
               </p>
               <div className="flex mb-[200px] ">
                 <Button>Пройти онлайн</Button>
-                <LightButton>Пройти оффлайн</LightButton>
+                <Link href="/international/cert/certpost"><LightButton>Пройти оффлайн</LightButton></Link>
+                
               </div>
             </div>
             <div className="hidden md:col-span-5 md:block">
